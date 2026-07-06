077ea5d931a38c0a29527b0922ac4625b4b68a8d
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-07-06 12:48:03 -0300

    add- ex 05 ok

--- ex_05.php
+++ ex_05.php
@@ -1,5 +1,30 @@
 <?php
 
-function analisarTexto(){
-    
+function analisarTexto($texto){
+
+$totalPalavras = str_word_count($texto, 0);
+
+$totalCaracteres = mb_strlen($texto);
+
+$textoMinusculo = mb_strtolower($texto);
+
+$totalVogais = preg_match_all('/[aeiouáéíóúâêîôûãõàèìòù]/u', $textoMinusculo);
+
+$totalConsoantes = preg_match_all('/[bcdfghjklmnpqrstvwxyz]/', $textoMinusculo);
+
+return [
+    'palavras' => $totalPalavras,
+    'caracteres' => $totalCaracteres,
+    'vogais' => $totalVogais,
+    'consoantes' => $totalConsoantes
+  ];
 }
+
+$meuTexto = "O PHP e incrivel para WEB!";
+$resultado = analisarTexto($meuTexto);
+
+echo "Texto analisado: \"$meuTexto\"<br><br>";
+echo "Quantidade de palavras: " . $resultado['palavras'] . "<br>";
+echo "Quantidade de caracteres: " . $resultado['caracteres'] . "<br>";
+echo "Quantidade de vogais: " . $resultado['vogais'] . "<br>";
+echo "Quantidade de consoantes: " . $resultado['consoantes'] . "<br>";
