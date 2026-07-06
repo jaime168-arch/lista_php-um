1a42633c46181f383952306a772e5d3fef73ef34
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-07-06 10:53:01 -0300

    add- primeiro exercicio ok

---
+++ ex_01.php
@@ -0,0 +1,14 @@
+<?php
+
+function calcularFormula(x,y){
+//$numer0 = 1;
+//$Letra = "i";
+  if($x + $y) == 0{
+    return "Não é possível dividir por 0!";
+   }
+
+
+
+}
+
+echo calcularFormula(1,4);
