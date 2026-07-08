bbde6089f4143c349d999e0799f996baa4bf523f
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-07-08 09:33:21 -0300

    add- ex 09 ok

--- ex_09.php
+++ ex_09.php
@@ -1,24 +1,47 @@
 <?php
 
 function analisarNumero($numero) {
-$numero = (int)$numero;
+    $numero = (int)$numero;
 
-$parImpar = ($numero % 2 == 0) ? "Par" : "Ímpar";
+    $parImpar = ($numero % 2 == 0) ? "Par" : "Ímpar";
 
-$ePrimo = true
-if ($numero <=1){
-    $ePrimo = false;
-} else {
-    for ($i = 2; $i <= sqrt($numero); $i++) {
-        if ($numero % $i == 0) {
-            $ePrimo = false;
-            break;
+    $ePrimo = true; 
+    if ($numero <= 1){
+        $ePrimo = false;
+    } else {
+        for ($i = 2; $i <= sqrt($numero); $i++) {
+            if ($numero % $i == 0) {
+                $ePrimo = false;
+                break;
+            }
         }
     }
-}
-$primoResultado = $ePrimo ? "É Primo" : "Não é Primo";
+    $primoResultado = $ePrimo ? "É Primo" : "Não é Primo";
 
-$somaDivisores = 0;
+    $somaDivisores = 0;
 
-for ($i)
+    for ($i = 1; $i < $numero; $i++){
+        if ($numero % $i == 0) {
+            $somaDivisores += $i;
+        }
+    }
+
+    $ePerfeito = ($somaDivisores == $numero && $numero > 0);
+    $perfeitoResultado = $ePerfeito ? "É perfeito" : "Imperfeito";
+
+    return [
+        'par_impar' => $parImpar, 
+        'primo' => $primoResultado,
+        'perfeito' => $perfeitoResultado
+    ];
+
 }
+
+$numTeste = 67; 
+$analise = analisarNumero($numTeste);
+
+echo "Análise do número {$numTeste}:\n";
+echo "- Classificação: " . $analise['par_impar'] . "\n";
+echo "- Status Primo: " . $analise['primo'] . "\n";
+echo "- Status Perfeito: " . $analise['perfeito'] . "\n";
+
