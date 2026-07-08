ee2645c26d07c20cc719cb668fb62ace5582d426
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-07-08 08:08:17 -0300

    add- ex 07 ok

--- ex_07.php
+++ ex_07.php
@@ -1,3 +1,12 @@
 <?php
 
-function calcularDesconto(){}
+function calcularDesconto($valorNormal, $porcentagemDesconto){
+  $valorDescontado = $valorNormal * ($porcentagemDesconto / 100);
+  return $valorNormal - $valorDescontado;
+}
+
+$produtoA = calcularDesconto(100.00, 10); // 100 reais com 10% de desconto
+$produtoB = calcularDesconto(670.67, 20); // 670,67 reais com 20% de desconto
+
+echo "Produto A: R$ " . number_format($produtoA, 2, ',', '.') . "\n";// 90 reais
+echo "Produto B: R$ " . number_format($produtoB, 2, ',', '.') . "\n";// 536,54 reais
